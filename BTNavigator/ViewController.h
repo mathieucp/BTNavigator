4bbfb620fa09790e0db266c329e77c6aab2f2358
Tu Nguyen <mathieucp@gmail.com>
2015-07-10 21:04:17 +0700

    Init Project Navigator

---
+++ BTNavigator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BTNavigator
+//
+//  Created by Tu Nguyen on 7/10/15.
+//  Copyright (c) 2015 TDStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BTNavigator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BTNavigator
+//
+//  Created by Tu Nguyen on 7/10/15.
+//  Copyright (c) 2015 TDStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
